213199696db9497c42eab5154961734c16696609
josex2r <codex2r@gmail.com>
2015-10-19 23:29:45 +0200

    Sync promise

--- app/pods/ipc/service.js
+++ app/pods/ipc/service.js
@@ -12,12 +12,16 @@ export default Ember.Service.extend({
     stack: [],
 
     init: function(){
-        this.get('ipc').on(this.get('responseName'), function(data){
-            data = JSON.parse(data);
+        this.get('ipc').on(this.get('responseName'), function(response){
+            response = JSON.parse(response);
             this.get('stack').forEach(function(promise){
-                if(promise.request.timestamp === data.timestamp &&
-                   promise.request.name === data.name){
-                    promise.deferred.resolve(data);
+                if(promise.request.timestamp === response.timestamp &&
+                   promise.request.name === response.name){
+                    if(response){
+                        promise.deferred.resolve(response);
+                    }else{
+                        promise.deferred.reject();
+                    }
                 }
             });
         }.bind(this));
@@ -35,44 +39,49 @@ export default Ember.Service.extend({
         var reqType = this.get('ipc')[request.sync ? 'sendSync' : 'send'];
         reqType(this.get('requestName'), JSON.stringify(request.serialize()));
 
+        var deferred = new Ember.RSVP.defer(),
+            promise = deferred.promise,
+            timeout = setTimeout(function(){
+                // Reject promise
+                deferred.reject('timeout');
+            }, request.get('timeout'));
+
+        // Set promise data
+        promise.request = request;
+        promise.deferred = deferred;
+
+        promise.then(function(response){
+            Ember.Logger.info('IPC | Response for ', promise.request, ' => ', response);
+        }).catch(function(error){
+            switch(error){
+                case 'timeout':
+                    Ember.Logger.error('IPC | Timeout for ', request);
+                    break;
+                default:
+                    Ember.Logger.error('IPC | Error for ', request);
+            }
+        }).finally(function(){
+            if(timeout){
+                clearTimeout(timeout);
+            }
+            // Remove from stack
+            this.get('stack').removeObject(promise);
+        }.bind(this));
+
         if(!request.sync){
             this.get('ipc').send(this.get('requestName'), JSON.stringify(request.serialize()));
 
-            var deferred = new Ember.RSVP.defer(),
-                promise = deferred.promise,
-                timeout = setTimeout(function(){
-                    // Reject promise
-                    deferred.reject('timeout');
-                }, request.get('timeout'));;
-
-            // Set promise data
-            promise.request = request;
-            promise.deferred = deferred;
-
-            promise.then(function(data){
-                Ember.Logger.info('IPC | Response for ', promise.request);
-            }).catch(function(error){
-                switch(error){
-                    case 'timeout':
-                        Ember.Logger.error('IPC | Timeout for ', request);
-                        break;
-                    default:
-                        Ember.Logger.error('IPC | Error for ', request);
-                }
-            }).finally(function(){
-                if(timeout){
-                    clearTimeout(timeout);
-                }
-                // Remove from stack
-                this.get('stack').removeObject(promise);
-            }.bind(this));
-
             this.get('stack').addObject(promise);
-
-            return promise;
         }else{
-            return this.get('ipc').sendSync(this.get('requestName'), JSON.stringify(request.serialize()));
+            var response = this.get('ipc').sendSync(this.get('requestName'), JSON.stringify(request.serialize()));
+            if(response){
+                deferred.resolve(response);
+            }else{
+                deferred.reject();
+            }
         }
+
+        return promise;
 
     }
 
